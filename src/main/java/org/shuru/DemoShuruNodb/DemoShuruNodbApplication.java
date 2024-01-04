854ea7da1a6ce589444fd594f5ca5751e4d8f1c9
SeminDEV <bonokbs@naver.com>
2024-01-04 22:34:52 +0900

    최초 수정

---
+++ src/main/java/org/shuru/DemoShuruNodb/DemoShuruNodbApplication.java
@@ -0,0 +1,13 @@
+package org.shuru.DemoShuruNodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoShuruNodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoShuruNodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/shuru/DemoShuruNodb/DemoShuruNodbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shuru.DemoShuruNodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoShuruNodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
